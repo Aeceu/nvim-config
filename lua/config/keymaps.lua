3d4dbd2a72d8e2b4f32120f77192b1d8751d77b7
kaneki081 <joseacebuche2@gmail.com>
2025-04-13 23:08:43 +0800

    added lualine style

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+-- Word wrap
+
+vim.keymap.set("n", "<leader>cw", function()
+  vim.wo.wrap = not vim.wo.wrap
+  vim.cmd("redrawstatus") -- force lualine to refresh
+  print("Wrap: " .. (vim.wo.wrap and "ON" or "OFF"))
+end, { desc = "Toggle Word Wrap" })
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "ayu_dark",
+          section_separators = "",
+          component_separators = "|",
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = {
+            {
+              "branch",
+              icon = "",
+              fmt = function(str)
+                return "branch: " .. str
+              end,
+            },
+            "diff",
+          },
+          lualine_c = {
+            "filename",
+          },
+          lualine_x = {
+            {
+              "diagnostics",
+              sources = { "nvim_diagnostic" },
+              symbols = { error = " ", warn = " ", info = " " },
+            },
+          },
+          lualine_y = { "filetype" },
+          lualine_z = { "location" },
+        },
+      })
+    end,
+  },
+}
